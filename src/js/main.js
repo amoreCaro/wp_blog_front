0c27849a7a1fbdaead210d0c062ae3a2c026267f
amoreCaro <admin@dynamic-005-005-005-230.5.5.pool.telefonica.de>
2026-01-20 14:16:34 +0200

    Create header (frontend): header popup search layout and styling

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,40 @@
 import { estimateSinglePostReadTime } from "./components/estimateSinglePostReadTime.js";
-import { calculateTotalPages} from "./components/calculateTotalPages.js";
+import { calculateTotalPages } from "./components/calculateTotalPages.js";
 
 document.addEventListener('DOMContentLoaded', function() {
-  console.log("Main js loaded")
+  console.log("Main js loaded");
+  const openBtn = document.getElementById('openSearch');
+  const closeBtn = document.getElementById('closeSearch');
+  const popup = document.getElementById('searchPopup');
+  const searchPopupOverlay = document.getElementById('searchPopupOverlay');
+
+  openBtn.addEventListener('click', () => {
+    console.log("clicked open button");
+    popup.classList.remove('hidden');
+    popup.classList.add('flex');
+    document.body.classList.add("overflow-y-hidden");
+  });
+
+  closeBtn.addEventListener('click', () => {
+    popup.classList.add('hidden');
+    popup.classList.remove('flex');
+    document.body.classList.remove("overflow-y-hidden");
+  });
+
+  searchPopupOverlay.addEventListener('click', () => {
+    popup.classList.add('hidden');
+    popup.classList.remove('flex');
+    document.body.classList.remove("overflow-y-hidden");
+  });
+
+  // Закриття при натисканні поза контейнером
+  popup.addEventListener('click', (e) => {
+    if (e.target === popup) {
+        popup.classList.add('hidden');
+        popup.classList.remove('flex');
+    }
+  });
+
   calculateTotalPages();
   estimateSinglePostReadTime();
 });
